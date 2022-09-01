6f2b3074f7557446ef9a4c630dbb68ab135037a9
Corban-Lee <843224@student.derby-college.ac.uk>
2022-09-01 23:27:11 +0100

    rewrote a small functional bot
    has logging capabilities

--- src/main.py
+++ src/main.py
@@ -2,39 +2,64 @@
 Entry point for the bot. Run this file to get things started.
 """
 
+import os
 import logging
+import logging.handlers
 import discord
 from discord.ext import commands
 
 
-log = logging.getLogger(__name__)
-discord.utils.setup_logging()
-
-
-class Bot(commands.Bot):
+def setup_logging():
     """
-    Bot class is the heart of operations
+    Setup logging for the bot.
     """
-
-    def __init__(self):
-        super().__init__(
-            command_prefix='>',
-            intents=discord.Intents.all(),
-        )
-        
-    async def on_ready(self):
-        msg = 'Logged in as {0} (ID: {0.id})'.format(self.user)
-        log.info(msg)
-        
-    async def load_cog(self):
-        pass
     
-    async def unload_cog(self):
-        pass
+    # Setup logging
+    log = logging.getLogger('discord')
+    log.setLevel(logging.DEBUG)
+    logging.getLogger('discord.http').setLevel(logging.INFO)
 
-    def run(self):
-        self.start(token='')
-        
+    # Create the log file handler
+    handler = logging.handlers.RotatingFileHandler(
+        filename='logs/discord.log',
+        encoding='utf-8',
+        maxBytes=32 * 1024 * 1024,  # 32MB
+        backupCount=5
+    )
+
+    # Formatter for the log files
+    datetime_format = '%d-%m-%Y %H:%M:%S'
+    formatter = logging.Formatter(
+        '[{asctime}] [{levelname:<8}] {name}: {message}', 
+        datetime_format, style='{'
+    )
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    return log
 
 if __name__ == '__main__':
-    Bot().run()
+    log = setup_logging()
+    
+    # Create the bot
+    bot = commands.Bot(
+        command_prefix='!', intents=discord.Intents().all()
+    )
+    bot.remove_command('help')  # we wont be needing this
+    
+    # Load the cogs
+    for filename in os.listdir('cogs/'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
+            
+    @bot.event
+    async def on_ready():
+        """
+        Call this function when the bot is ready to receive commands
+        """
+        output = f'Logged in as {bot.user} (ID: {bot.user.id})'
+        log.info(output)
+
+    # Startup the bot using the hidden token
+    with open('TOKEN', 'r', encoding='utf-8') as f:
+        token = f.read()
+    bot.run(token=token)
